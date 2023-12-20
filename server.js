a1e390ce607c227c95773099f4cbf2e666856d97
Maciek Skokowski <maciejskok2004@gmail.com>
2023-12-20 19:12:34 +0100

    added responsbility and step 2 of game

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// rules section
 const rulesBtn = document.getElementById('rulesBtn');
 const closeRulesBtn = document.getElementById('closeRulesBtn');
 
@@ -17,3 +18,34 @@ function closeRules () {
 
 rulesBtn.addEventListener('click', toggleRules);
 closeRulesBtn.addEventListener('click', closeRules);
+
+// results section
+const pickedContainer = document.getElementById('pickedContainer');
+const resultsBtn = document.getElementById('resultsBtn');
+const mainContainer = document.getElementById('mainContainer');
+const allGameBtn = document.querySelectorAll('#gameBtn');
+const resultsText = document.getElementById('resultsText');
+
+function showWhatYouPicked (e) {
+  const btnClicked = e.target;
+  const btnClickedValue = btnClicked.dataset.value;
+  const pickedLeftSide = document.getElementById('pickedLeftSide');
+  const createDiv = document.createElement('div');
+  createDiv.classList.add(`${btnClickedValue}-btn-container`, 'btn-container', 'bigger');
+  createDiv.innerHTML = `
+    <button class="${btnClickedValue}-btn game-btn">
+      <img class="btn-img-icon" src="/images/icon-${btnClickedValue}.svg" alt="${btnClickedValue}-icon">
+    </button>
+  `;
+  resultsText.textContent = 'You picked ' + btnClickedValue;
+  pickedLeftSide.append(createDiv);
+
+  mainContainer.classList.remove('active');
+  pickedContainer.classList.add('active');
+}
+
+allGameBtn.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    showWhatYouPicked(e);
+  });
+});
